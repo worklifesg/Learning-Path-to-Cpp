31d40f90d24f05df8a61b4b40dc627dc85caf58d
Shraman Gupta <worklife_sg@outlook.com>
2021-01-12 15:24:52 -0500

    Create 1_1_simple_program.cpp

---
+++ Course 1 - Learning C++/1_1_simple_program.cpp
@@ -0,0 +1,13 @@
+//Hello world program
+/* By Shraman Gupta */
+
+#include <iostream> //input output stream
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+}
+
+// cout - console output operator
+// endl - add new line to the console
