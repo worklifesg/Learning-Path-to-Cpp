9ee99acee4708566084bf8b4f62e672617860785
Shraman Gupta <worklife_sg@outlook.com>
2021-01-12 15:25:30 -0500

    Create 1_2_HelloWorld.cpp

---
+++ Course 1 - Learning C++/1_2_HelloWorld.cpp
@@ -0,0 +1,16 @@
+//Hello world program
+/* By Shraman Gupta */
+
+#include <iostream> //input output stream
+using namespace std;
+
+int main()
+{
+	string name; //variable name
+	cout <<"Please enter your name: " << endl;
+	cin >> name; //input name console(cin)
+	cout << "hello, " << name << endl;
+}
+
+// cout - console output operator
+// endl - add new line to the console
