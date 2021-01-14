0974db7b1dad76b280a10063d42d1d8e0f6c4f08
Shraman Gupta <worklife_sg@outlook.com>
2021-01-14 13:36:33 -0500

    Chapter 3 - For Loop Example 'Hour Loop'

---
+++ Course 1 - Learning C++/3_4_HourLoop.cpp
@@ -0,0 +1,42 @@
+// Hour Loop using 'for' loop
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	for(int h =0; h < 24; h++)
+	{
+		cout << h << ":00\n";
+	}
+}
+
+/*
+0:00
+1:00
+2:00
+3:00
+4:00
+5:00
+6:00
+7:00
+8:00
+9:00
+10:00
+11:00
+12:00
+13:00
+14:00
+15:00
+16:00
+17:00
+18:00
+19:00
+20:00
+21:00
+22:00
+23:00
+*/
